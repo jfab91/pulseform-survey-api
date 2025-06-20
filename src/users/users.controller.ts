1115905b0e2391617b17c253fed0216a84af0b76
Fabricio Vargas <fabricio.vargas@gamanza.com>
2025-06-20 12:47:16 -0600

    fix: add http verbs to user endpoints

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
@@ -8,10 +8,12 @@ import { User } from './schemas/user.schema';
 export class UsersController {
   constructor(private readonly service: UsersService) {}
 
+  @Post()
   async create(@Body() dto: CreateUserDto): Promise<User> {
     return this.service.create(dto);
   }
 
+  @Get()
   async findAll(): Promise<User[]> {
     return this.service.findAll();
   }
