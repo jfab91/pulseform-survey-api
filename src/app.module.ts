f14b3d7a6bc52b2e3a77ca55198a514febc56638
Fabricio Vargas <fabricio.vargas@gamanza.com>
2025-06-20 09:52:50 -0600

    chore: add basic modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { SurveysModule } from './surveys/surveys.module';
+import { QuestionsModule } from './questions/questions.module';
+import { ResponsesModule } from './responses/responses.module';
+import { AnalyticsModule } from './analytics/analytics.module';
+import { CommonModule } from './common/common.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    AuthModule,
+    UsersModule,
+    SurveysModule,
+    QuestionsModule,
+    ResponsesModule,
+    AnalyticsModule,
+    CommonModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
