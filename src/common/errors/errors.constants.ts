c4cd10d73ad11f52bd5e32ffd707b9f9012f9aa6
Fabricio Vargas <fabricio.vargas@gamanza.com>
2025-06-21 12:58:15 -0600

    chore: add file with error constants

---
+++ src/common/errors/errors.constants.ts
@@ -0,0 +1,16 @@
+export const Errors = {
+  USER: {
+    EXISTS: 'A user with this email already exists',
+    NOT_FOUND: 'User not found',
+  },
+  AUTH: {
+    INVALID_CREDENTIALS: 'Invalid credentials',
+    UNAUTHORIZED: 'Unauthorized access',
+  },
+  SURVEY: {
+    NOT_FOUND: 'Survey not found',
+  },
+  QUESTION: {
+    NOT_FOUND: 'Question not found',
+  },
+};
