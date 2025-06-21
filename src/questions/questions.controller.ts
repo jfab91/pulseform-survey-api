ff0fed92e096b0ba48463d47e82a13bd74246ca1
Fabricio Vargas <fabricio.vargas@gamanza.com>
2025-06-21 12:59:41 -0600

    feat: add implementation for editing and removing questions from a survey

--- src/questions/questions.controller.ts
+++ src/questions/questions.controller.ts
@@ -1,9 +1,18 @@
-import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 import { RolesGuard } from '../auth/guards/roles.guard';
 import { QuestionsService } from './questions.service';
-import { CreateQuestionDto } from './dto';
+import { CreateQuestionDto, UpdateQuestionDto } from './dto';
 import { Question } from './schemas/question.schema';
 import { UserRole } from '../users/enums';
 import { Roles } from '../auth/decorators/roles.decorator';
@@ -26,5 +35,20 @@ export class QuestionsController {
   @Roles(UserRole.ADMIN, UserRole.CREATOR)
   findAll(@Param('surveyId') surveyId: string) {
     return this.service.findBySurvey(surveyId);
+  }
+
+  @Patch(':questionId')
+  @Roles(UserRole.ADMIN, UserRole.CREATOR)
+  update(
+    @Param('questionId') questionId: string,
+    @Body() dto: UpdateQuestionDto,
+  ) {
+    return this.service.update(questionId, dto);
+  }
+
+  @Delete(':questionId')
+  @Roles(UserRole.ADMIN, UserRole.CREATOR)
+  remove(@Param('questionId') questionId: string) {
+    return this.service.delete(questionId);
   }
 }
--- src/questions/questions.service.ts
+++ src/questions/questions.service.ts
@@ -1,9 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 
 import { Question, QuestionDocument } from './schemas/question.schema';
-import { CreateQuestionDto } from './dto';
+import { CreateQuestionDto, UpdateQuestionDto } from './dto';
+import { Errors } from '../common/errors';
 
 @Injectable()
 export class QuestionsService {
@@ -19,5 +20,21 @@ export class QuestionsService {
 
   async findBySurvey(surveyId: string): Promise<Question[]> {
     return this.model.find({ survey: surveyId }).exec();
+  }
+
+  async update(questionId: string, dto: UpdateQuestionDto): Promise<Question> {
+    const updated = await this.model
+      .findByIdAndUpdate(questionId, dto, { new: true })
+      .exec();
+
+    if (!updated) throw new NotFoundException(Errors.QUESTION.NOT_FOUND);
+
+    return updated;
+  }
+
+  async delete(questionId: string): Promise<void> {
+    const result = await this.model.findByIdAndDelete(questionId);
+
+    if (!result) throw new NotFoundException(Errors.QUESTION.NOT_FOUND);
   }
 }
