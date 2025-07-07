565c9a2c0edcbb7eb38c862402b4834ff89b1e03
Fabricio Vargas <fabricio.vargas@gamanza.com>
2025-07-07 15:55:19 -0600

    feat: add swagger support for auth module

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,25 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { AuthResponseDto } from './dto/responses/auth-response.dto';
 
+@ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly service: AuthService) {}
 
   @Post('login')
+  @ApiOperation({ summary: 'User Login' })
+  @ApiResponse({
+    status: 200,
+    description: 'User logged in successfully',
+    type: AuthResponseDto,
+  })
+  @ApiResponse({
+    status: 401,
+    description: 'Invalid credentials',
+  })
   async login(@Body() dto: LoginDto) {
     return this.service.login(dto.email, dto.password);
   }
